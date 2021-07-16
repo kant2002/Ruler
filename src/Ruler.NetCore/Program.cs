48b7be9f0ca4248255caacd18432d9995af5009a
Andrii Kurdiumov <kant2002@gmail.com>
2021-07-16 17:54:05 +0600

    Compile using NativeAOT

--- src/Ruler.NetCore/Program.cs
+++ src/Ruler.NetCore/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,6 +15,7 @@ namespace Ruler.NetCore
         [STAThread]
         static void Main()
         {
+            ComWrappers.RegisterForMarshalling(WinFormsComInterop.WinFormsComWrappers.Instance);
             Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
